19da7962794707b97fd39cab88aefd23cd3ac6ec
orhankalayci2017 <orhan@kalayci.com>
2022-05-10 21:11:13 +0300

    initial commit

---
+++ FibonacciNumberGenerator/FibonacciNumberGeneratorClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace FibonacciNumberGenerator
+{
+    internal class FibonacciNumberGeneratorClass
+    {
+        public FibonacciNumberGeneratorClass()
+        {
+        }
+
+        private bool checkIfIndexValid(int index)
+        {
+            return (index >= 0);
+        }
+
+        internal int errorCode()
+        {
+            return -321;
+        }
+
+        internal int getFibonnaci(int index)
+        {
+            if (!checkIfIndexValid(index)) return errorCode();
+            return fibonnaciNumberCalculatingBusinessRules(index);
+
+        }
+
+
+        internal int fibonnaciNumberCalculatingBusinessRules(int index)
+        {
+
+            int fibonacciNumber = 0 ;
+
+            if (index == 0) { fibonacciNumber = 0; return fibonacciNumber; }
+            if (index == 1) { fibonacciNumber = 1; return fibonacciNumber; }
+
+
+            int twoBefore = 0;
+            int oneBefore = 1;
+
+            while (index-- > 1)
+            {
+                fibonacciNumber = oneBefore + twoBefore;
+
+                twoBefore = oneBefore;
+                oneBefore = fibonacciNumber;
+
+            }
+
+            return fibonacciNumber;
+
+        }
+
+
+    }
+}
---
+++ FibonacciNumberGenerator/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace FibonacciNumberGenerator
+{
+    public class FibonacciTests
+    {
+
+        [Timeout (200)]
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(2, 1)]
+        [TestCase(3, 2)]
+        [TestCase(4, 3)]
+        [TestCase(5, 5)]
+        [TestCase(6, 8)]
+        [TestCase(7, 13)]
+        [TestCase(8, 21)]
+        [TestCase(44, 701408733)]
+
+        public void validIndex(int index, int expected)
+        {
+            var fibonnaciInstance = new FibonacciNumberGeneratorClass();
+
+            Assert.AreEqual(expected, fibonnaciInstance.getFibonnaci(index));
+
+        }
+
+        [TestCase(-1, -321)]
+
+        public void inValidIndex(int index, int expected)
+        {
+            var fibonnaciInstance = new FibonacciNumberGeneratorClass();
+
+            Assert.AreEqual(expected, fibonnaciInstance.getFibonnaci(index));
+
+        }
+
+    }
+}
